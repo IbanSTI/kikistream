03d7a288207af6942bc748de9d221ff3e37c4060
szigh <szigh@0920144g.peda>
2024-11-08 17:07:16 +0100

    Ajout de la page de selmen

---
+++ Part_Selmen/Histoire.js
@@ -0,0 +1,17 @@
+/* Partie Navbar */
+let sidenav = document.getElementById("Hamburger");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    sidenav.classList.remove("active");
+}
